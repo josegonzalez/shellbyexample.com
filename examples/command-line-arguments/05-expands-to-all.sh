534db8388a7de443d0ba4e282c957b63b64dd016
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2026-01-18 11:28:41 -0500

    chore: fix example output

--- examples/command-line-arguments/05-expands-to-all.sh
+++ examples/command-line-arguments/05-expands-to-all.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
-# `$@` expands to all arguments as separate words.
-# `$*` expands to all arguments as a single word when quoted.
+# - `$@` expands to all arguments as separate words.
+# - `$*` expands to all arguments as a single word when quoted.
 
-echo "All arguments (\$@):"
-for arg in "$@"; do
-    echo "  - $arg"
-done
+cmd_with_args() {
+    echo "All arguments (\$@):"
+    for arg in "$@"; do
+        echo "  - $arg"
+    done
 
-echo "All arguments (\$*):"
-echo "  $*"
+    echo "All arguments (\$*):"
+    echo "  $*"
+}
+
+cmd_with_args a b c d e
